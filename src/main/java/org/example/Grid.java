7e07788912d31c8fa997071b1d31a84fab1068b2
estaruma <esther.arumainayagam@mettle.co.uk>
2024-08-20 16:28:56 +0100

    Second commit

--- src/main/java/org/example/Grid.java
+++ src/main/java/org/example/Grid.java
@@ -16,21 +16,41 @@ public class Grid {
         this.rows = rows;
         this.columns = columns;
         cells = new Cell[rows][columns];
-    }
 
-    // intialise each cell in grid
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            cells[i][j] = new Cell();
+        // intialise each cell in grid
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                cells[i][j] = new Cell();
+            }
         }
     }
 
+    // get specific cell in the grid
+    public Cell getCell(int row, int column) {
+        if (row >= 0 && row < rows && column >= 0 && column < columns) {
+            return cells[row][column];
+        }
+        return null;
+    }
 
+    // count living neighbours
 
+    public int countAliveNeighbours(int row, int column) {
+        int aliveNeighbours = 0;
 
+        // loop through cells
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                Cell neighbour = getCell((row), column);
+                if (neighbour != null && neighbour.isAlive()) {
+                    aliveNeighbours++;
+                }
+            }
 
-
-
-
+        }
+        return aliveNeighbours;
+    }
 
 }
+
+
